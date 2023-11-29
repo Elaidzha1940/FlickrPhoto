23332a0ecfce2eccf55a83fe723376d743108b41
Elaidzha1940 <eli080293@gmail.com>
2023-11-29 09:54:54 +0300

    FlickrPhoto

---
+++ FlickrPhoto/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: FlickrPhoto
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlickrPhoto/FlickrPhotoApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: FlickrPhoto
+//  File: FlickrPhotoApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.11.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct FlickrPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
